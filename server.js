bf8edfd1b1c617ccf73a74d000890a9756193acd
bryce <blarkin13@gmail.com>
2022-03-11 01:49:06 -0600

    start routes folder

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+//const apiRoutes = require('./routes/apiRoutes')
+//const htmlRoutes = require('./routes/htmlRoutes')
+
 const fs = require('fs');
 const express = require("express");
 const path = require("path");
@@ -6,20 +9,25 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
+// parse incoming string or array
 app.use(express.urlencoded({ extended: true }));
+// parse incoming json data
 app.use(express.json());
 
+//app.use('/api', apiRoutes);
+//app.use('/', htmlRoutes);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
 app.get("/notes", (req, res) => {
-    //res.send('hello');
+    res.send('hello');
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "db/db.json"));
-});
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.post("/api/notes", (req, res) => {
