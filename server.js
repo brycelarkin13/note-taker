c1de1b3cdd9aef7e70200bb2eac3d218c81aa9bc
bryce <blarkin13@gmail.com>
2022-03-19 19:51:39 -0500

    corrected file paths

--- server.js
+++ server.js
@@ -13,35 +13,38 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming json data
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.get("/notes", (req, res) => {
     //res.send('hello');
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+    res.sendFile(path.join(__dirname, '/db/db.json'));
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.post("/api/notes", (req, res) => {
+app.post('/api/notes', (req, res) => {
     let newNote = req.body;
-    let noteList = JSON.parse(fs.readFileSync("/db/db.json", "utf8"));
+    let noteList = JSON.parse(fs.readFileSync('./db/db.json', "utf8"));
     let notelength = (noteList.length).toString();
 
     newNote.id = notelength;
     noteList.push(newNote);
 
-    fs.writeFileSync("/db/db.json", JSON.stringify(noteList));
+    fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
     res.json(noteList);
 });
 
 // note delete method
-app.delete("/api/notes/:id", (req, res) => {
-    let noteList = JSON.parse(fs.readFileSync("/db/db.json", "utf8"));
+app.delete('/api/notes/:id', (req, res) => {
+    let noteList = JSON.parse(fs.readFileSync('./db/db.json', "utf8"));
     let noteId = (req.params.id).toString();
 
     // filter through all notes to find Id match to delete specific Id
@@ -50,11 +53,11 @@ app.delete("/api/notes/:id", (req, res) => {
     })
 
     // writes the new updated file to JSON
-    fs.writeFileSync("/db/db.json", JSON.stringify(noteList));
+    fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
     res.json(noteList);
 });
 
 // method to listen for which port the app is running on
 app.listen(PORT, () => {
-    console.log('Server now on port ${PORT}!');
+    console.log(`Server now on port ${PORT}!`);
 });
